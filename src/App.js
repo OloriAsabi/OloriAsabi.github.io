6d89d2d6d2c526f788e844f4d81cb6fbca796ec2
Ashabi L Oduntan <latyphab@gmail.com>
2021-10-17 14:26:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Skills  from "./components/MySkills";
 function App() {
   return(
     <Router>
+    <div>
       <Navbar/>
       <Switch>
         <Route exact path="/">
@@ -29,6 +30,7 @@ function App() {
           Contact
           </Route>
       </Switch>
+    </div>
       </Router>
   )
 }
