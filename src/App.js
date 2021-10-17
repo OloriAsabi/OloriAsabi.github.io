48cc851c623dc2f3367b5518a8c9bad8e97591f4
Latifat Oduntan <79084252+latifa-12@users.noreply.github.com>
2021-10-17 17:08:11 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,25 +4,18 @@ import { BrowserRouter as Router,
   Route,
  } from "react-router-dom";
 
-import Navbar from './components/Navbar';
 
-import Contact from './components/Contact';
+
+
 import Home from './components/Home';
 import About from "./components/About";
 import Skills  from "./components/MySkills";
-<<<<<<< HEAD
 import Contact from "./components/Contact";
 import Navbar from './components/Navbar';
-=======
->>>>>>> f0f3c8d5babfc37b893b385a191367591af7e0cc
 
 function App() {
   return(
     <Router>
-<<<<<<< HEAD
-=======
-    <div>
->>>>>>> f0f3c8d5babfc37b893b385a191367591af7e0cc
       <Navbar/>
       <Switch>
         <Route exact path="/">
@@ -34,18 +27,10 @@ function App() {
         <Route path="/skills">
           <Skills />
         </Route>
-<<<<<<< HEAD
         <Route path="/contact">
           <Contact />
         </Route>
       </Switch>
-=======
-        <Route path='/contact'>
-          Contact
-          </Route>
-      </Switch>
-    </div>
->>>>>>> f0f3c8d5babfc37b893b385a191367591af7e0cc
       </Router>
   )
 }
