f56a97ff2f3bd05c6f75cc8ec65b15866e5aa41f
OloriAsabi <developerqueenlatifa@gmail.com>
2022-02-09 17:00:40 +0100

    Updates

---
+++ precache-manifest.43fd5c6d2194fb0b311454c0f7a45b7e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef9f9fa46bbe87a8dec4adcea21090b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0511a40aeb944a9b06f9",
+    "url": "/static/css/2.81f1adc6.chunk.css"
+  },
+  {
+    "revision": "8c6e66691b91e3016500",
+    "url": "/static/css/main.853c742e.chunk.css"
+  },
+  {
+    "revision": "0511a40aeb944a9b06f9",
+    "url": "/static/js/2.186bdb67.chunk.js"
+  },
+  {
+    "revision": "8c6e66691b91e3016500",
+    "url": "/static/js/main.fce37fea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "810621ee0b98b2e3aed06efc938657f0",
+    "url": "/static/media/5.810621ee.png"
+  },
+  {
+    "revision": "b18da4794a99d1ba593b901d3d78a200",
+    "url": "/static/media/Ashabi_Lateefat_Oduntan_most recent.b18da479.pdf"
+  },
+  {
+    "revision": "75180220f6bd48204f4e14b600095bbe",
+    "url": "/static/media/olori.75180220.jpeg"
+  }
+]);
