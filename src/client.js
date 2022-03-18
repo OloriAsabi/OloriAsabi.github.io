1c4592246e0c88114a412c96199b5c07d82c77b6
OloriAsabi <developerqueenlatifa@gmail.com>
2022-03-18 10:55:32 +0100

    commit on 'main'

--- src/client.js
+++ src/client.js
@@ -6,7 +6,7 @@ export const client = sanityClient({
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
-    token: 'skgqk8xraywLL7UMTHum8WbC8kZo4FoQSkIhPnqrm7ql22LZ58MZlwhxxhxIMatrZJCILlL5TFrUJXc2wudQKlk6Or8NQ9sOpHp0GlTyYKCfLtnzp1m3o3dXMPONR2j4CCEc36BL3qpQbsrQ5pZamNtsOEne2imTLABnkhm635dKAtMYUYK7',
+    token: process.env.SANITY_TOKEN,
 });
 
 const builder = ImageUrlBuilder(client);
