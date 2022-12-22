c0d369b9bf55ac55461a52c408b63adeb2fc1828
OloriAsabi <developerqueenlatifa@gmail.com>
2022-12-22 13:57:36 +0100

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from '../images/1.png';
+import logo from '../images/olorii.png';
 const Navbar = () => {
   return (
     <div className="navbar" data-aos="fade-down">
       <div className="nav">
         <Link to="/" className="logo">
-          <img src={logo} alt="logo" className="w-12 h-12 object-cover image bg-white text-white rounded-none cursor-pointer" />
+          <img src={logo} alt="logo" className="w-12 h-12 object-cover image bg-transparent text-white rounded cursor-pointer" />
         </Link>
       </div>
       
